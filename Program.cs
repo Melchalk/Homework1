aa753498049a94708da634f994d6e3cd7db58d68
Марина Мельникова <melm2004@mail.ru>
2023-10-08 12:17:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Hello, World!");
+
+class Bag
+{
+    public int PizzaPieces { get; set; }
+    public int BeerBottles { get; set; }
+    public double Capacity { get; set; }
+    private const double pizzaInLiters = 0.3,
+        beerInLiters = 0.5;
+
+    public bool AddPizza(int pizzaPieces, Bag bag)
+    {
+        double countPizzaLiters = pizzaPieces * pizzaInLiters;
+        if (bag.Capacity - countPizzaLiters >= 0)
+        {
+            bag.Capacity -= countPizzaLiters;
+            return true;
+        }
+        return false;
+    }
+
+    public bool AddBeer(int beerBottles, Bag bag)
+    {
+        double countBeerLiters = beerBottles * beerInLiters;
+        if (bag.Capacity - countBeerLiters >= 0)
+        {
+            bag.Capacity -= countBeerLiters;
+            return true;
+        }
+        return false;
+    }
+}
