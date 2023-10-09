b6e69cc039645708f16df7a38c33c605c027236a
Марина Мельникова <melm2004@mail.ru>
2023-10-09 14:59:02 +0300

    Student done

--- Program.cs
+++ Program.cs
@@ -30,3 +30,39 @@ class Bag
         return false;
     }
 }
+
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Bag StudentBag { get; set; }
+    const double normalSatiety = 30;
+    const double normalDrunk = 72;
+    public string CountSatiety(Student student)
+    {
+        if (student.Age <= 0 || student.StudentBag.PizzaPieces <= 0)
+            return "Ошибка";
+        //уславливаемся, что с возрастом нужно меньше пиццы для насыщения
+        double levelSatiety = (student.StudentBag.PizzaPieces / student.Age) * 100;
+        if (levelSatiety < normalSatiety)
+            return "Голоден";
+        else if (levelSatiety > normalSatiety)
+            return "Переел";
+        else
+            return "В самый раз";
+    }
+
+    public string CountDrunk(Student student)
+    {
+        if (student.Age <= 0 || student.StudentBag.PizzaPieces <= 0)
+            return "Ошибка";
+        //уславливаемся, что с возрастом нужно больше пива для опьянения
+        double levelDrunk = student.StudentBag.PizzaPieces * student.Age;
+        if (levelDrunk < normalDrunk)
+            return "Трез";
+        else if (levelDrunk > normalDrunk)
+            return "Перепил";
+        else
+            return "В самый раз";
+    }
+}
