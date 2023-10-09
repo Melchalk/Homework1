f1d07650f036cf46acfd711025fec1558a35dd2f
Марина Мельникова <melm2004@mail.ru>
2023-10-09 15:02:05 +0300

    Namespace classes

---
+++ Bag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Bag
+    {
+        public int PizzaPieces { get; set; }
+        public int BeerBottles { get; set; }
+        public double Capacity { get; set; }
+        private const double pizzaInLiters = 0.3,
+            beerInLiters = 0.5;
+
+        public bool AddPizza(int pizzaPieces, Bag bag)
+        {
+            double countPizzaLiters = pizzaPieces * pizzaInLiters;
+            if (bag.Capacity - countPizzaLiters >= 0)
+            {
+                bag.Capacity -= countPizzaLiters;
+                return true;
+            }
+            return false;
+        }
+
+        public bool AddBeer(int beerBottles, Bag bag)
+        {
+            double countBeerLiters = beerBottles * beerInLiters;
+            if (bag.Capacity - countBeerLiters >= 0)
+            {
+                bag.Capacity -= countBeerLiters;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Bag StudentBag { get; set; }
+        const double normalSatiety = 30;
+        const double normalDrunk = 72;
+        public string CountSatiety(Student student)
+        {
+            if (student.Age <= 0 || student.StudentBag.PizzaPieces <= 0)
+                return "Ошибка";
+            //уславливаемся, что с возрастом нужно меньше пиццы для насыщения
+            double levelSatiety = (student.StudentBag.PizzaPieces / student.Age) * 100;
+            if (levelSatiety < normalSatiety)
+                return "Голоден";
+            else if (levelSatiety > normalSatiety)
+                return "Переел";
+            else
+                return "В самый раз";
+        }
+
+        public string CountDrunk(Student student)
+        {
+            if (student.Age <= 0 || student.StudentBag.PizzaPieces <= 0)
+                return "Ошибка";
+            //уславливаемся, что с возрастом нужно больше пива для опьянения
+            double levelDrunk = student.StudentBag.PizzaPieces * student.Age;
+            if (levelDrunk < normalDrunk)
+                return "Трез";
+            else if (levelDrunk > normalDrunk)
+                return "Перепил";
+            else
+                return "В самый раз";
+        }
+    }
+}
